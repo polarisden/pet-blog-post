b08bbdb36ac5e4fa2cfac72b6a801f0556392f99
polaris <dinsor1545@gmail.com>
2026-01-20 12:53:24 +0700

    feat: update NavBar component enhance navigation with clickable logo and sign-up button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,23 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
+import logoSvg from "@/assets/logo.svg"
+import menuSvg from "@/assets/navbar-menu.svg"
+import { useNavigate } from "react-router-dom"
 
 function NavBar(){
+  const navigate = useNavigate()
+
   return (
     <>
-      <div className="flex justify-between py-3 px-6 h-12 desktop:h-20 bg-brown-100 border-b border-brown-300 desktop:py-[16px] desktop:px-[120px]">
-        <img src="src\assets\logo.svg" className="w-6 h-6 desktop:w-11 desktop:h-11"/>
+      <div className="flex justify-between py-3 px-6 h-12 desktop:h-20 bg-brown-100 border-b border-brown-300 desktop:py-[16px] desktop:px-[120px] cursor-pointer">
+        <img src={logoSvg} className="w-6 h-6 desktop:w-11 desktop:h-11" alt="Logo" onClick={() => navigate('/')}/>
         
         {/* Mobile Dropdown Menu */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild className="desktop:hidden">
             <button className="w-6 h-6">
-              <img src="src\assets\navbar-menu.svg" className="w-6 h-6" alt="Menu"/>
+              <img src={menuSvg} className="w-6 h-6" alt="Menu"/>
             </button>
           </DropdownMenuTrigger>
           <DropdownMenuContent className="w-[375px] p-6" align="end">
@@ -38,8 +43,11 @@ function NavBar(){
 
         {/* Desktop Buttons */}
         <div className="gap-2 hidden desktop:flex">
-          <button className="desktop:h-[48px] desktop:w-[127px] desktop:border desktop:rounded-[999px] desktop:border-brown-400 desktop:text-body-1 desktop:text-brown-600">Log in</button>
-          <button className="desktop:h-[48px] desktop:w-[141px] desktop:rounded-[999px] desktop:bg-brown-600 desktop:text-body-1 desktop:text-white">Sign up</button>
+          <button className="desktop:h-[48px] desktop:w-[127px] desktop:border desktop:rounded-[999px] desktop:border-brown-400 desktop:text-body-1 desktop:text-brown-600 desktop:cursor-pointer desktop:transition-all desktop:duration-200 desktop:hover:bg-brown-300 desktop:hover:border-brown-500">Log in</button>
+          <button 
+            className="desktop:h-[48px] desktop:w-[141px] desktop:rounded-[999px] desktop:bg-brown-600 desktop:text-body-1 desktop:text-white desktop:cursor-pointer desktop:transition-all desktop:duration-200 desktop:hover:bg-brown-400"
+            onClick={() => navigate('/sign-up')}
+          >Sign up</button>
         </div> 
       </div>
     </>
